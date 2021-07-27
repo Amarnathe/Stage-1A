c0e092902e598250331ed98db9146a6e5c77541e
Amarnathe Katturaja <amarnathe.katturaja@telecom-paris.fr>
2021-07-27 18:47:37 +0200

    final version

--- Flask.py
+++ Flask.py
@@ -38,31 +38,185 @@ def index():
     Notes1 = []
 
     if "Recherche" in request.form.values():
-        for i in range(N):
 
-            if word != '':
-                if not(quotes[i] in quotes1) and ((str(word) in quotes[i]) or ((str(word) in str(quotes[i])) or (str(word) in str(authors[i])) or (str(word) in str(Source[i])) or (str(word) in str(Theme[i])))):
-                    if author != '':
+        if word!='' and author !='' and theme !='':
+            wordUp = word[0].capitalize() + word[1:]    # To consider the first capital letter
+            if Nmin != "" and Nmax != "" and Nmin <= Nmax:
+                for i in range(N):
+                    if not(quotes[i] in quotes1) and (((str(word) in quotes[i]) or ((str(word) in str(quotes[i])) or (str(word) in str(authors[i])) or (str(word) in str(Source[i])) or (str(word) in str(Theme[i])))) or ((str(wordUp) in quotes[i]) or ((str(wordUp) in str(quotes[i])) or (str(wordUp) in str(authors[i])) or (str(wordUp) in str(Source[i])) or (str(wordUp) in str(Theme[i]))))):
                         if author in str(authors[i]):
-                            if theme != '' and theme in str(Theme[i]):
-                                if not (quotes[i] in quotes1):
-                                    quotes1.append(quotes[i])
-                                    authors1.append(authors[i])
-                                    Source1.append(Source[i])
-                                    Theme1.append(Theme[i])
-                                    NbAvis1.append(NbAvis[i])
-                                    Notes1.append(Notes[i])
-                    elif theme != '':
-                        if theme in str(Theme[i]):
-                            if not (quotes[i] in quotes1):
+                            if theme in str(Theme[i]):
+                                if str(Notes[i]) != "nan":
+                                    if not (quotes[i] in quotes1) and Nmin<=str(int(Notes[i])) and Nmax>=str(int(Notes[i])):
+                                        quotes1.append(quotes[i])
+                                        authors1.append(authors[i])
+                                        Source1.append(Source[i])
+                                        Theme1.append(Theme[i])
+                                        NbAvis1.append(NbAvis[i])
+                                        Notes1.append(Notes[i])
+            else: #or "else"
+                for i in range(N):
+                    if not(quotes[i] in quotes1) and (((str(word) in quotes[i]) or ((str(word) in str(quotes[i])) or (str(word) in str(authors[i])) or (str(word) in str(Source[i])) or (str(word) in str(Theme[i])))) or ((str(wordUp) in quotes[i]) or ((str(wordUp) in str(quotes[i])) or (str(wordUp) in str(authors[i])) or (str(wordUp) in str(Source[i])) or (str(wordUp) in str(Theme[i]))))):
+                        if author in str(authors[i]):
+                            if theme in str(Theme[i]):
                                 quotes1.append(quotes[i])
                                 authors1.append(authors[i])
                                 Source1.append(Source[i])
                                 Theme1.append(Theme[i])
                                 NbAvis1.append(NbAvis[i])
                                 Notes1.append(Notes[i])
-                    else:
-                        if not (quotes[i] in quotes1):
+                        
+        elif word!='' and author !='' and theme =='':
+            wordUp = word[0].capitalize() + word[1:]    # To consider the first capital letter
+            if Nmin != "" and Nmax != "" and Nmin <= Nmax:
+                for i in range(N):
+                    if not(quotes[i] in quotes1) and (((str(word) in quotes[i]) or ((str(word) in str(quotes[i])) or (str(word) in str(authors[i])) or (str(word) in str(Source[i])) or (str(word) in str(Theme[i])))) or ((str(wordUp) in quotes[i]) or ((str(wordUp) in str(quotes[i])) or (str(wordUp) in str(authors[i])) or (str(wordUp) in str(Source[i])) or (str(wordUp) in str(Theme[i]))))):
+                        if author in str(authors[i]):
+                            if str(Notes[i])!="nan":
+                                if not (quotes[i] in quotes1) and Nmin<=str(int(Notes[i])) and Nmax>=str(int(Notes[i])): 
+                                    quotes1.append(quotes[i])
+                                    authors1.append(authors[i])
+                                    Source1.append(Source[i])
+                                    Theme1.append(Theme[i])
+                                    NbAvis1.append(NbAvis[i])
+                                    Notes1.append(Notes[i])
+            elif Nmin == "" and Nmax == "" :
+                for i in range(N):
+                    if not(quotes[i] in quotes1) and (((str(word) in quotes[i]) or ((str(word) in str(quotes[i])) or (str(word) in str(authors[i])) or (str(word) in str(Source[i])) or (str(word) in str(Theme[i])))) or ((str(wordUp) in quotes[i]) or ((str(wordUp) in str(quotes[i])) or (str(wordUp) in str(authors[i])) or (str(wordUp) in str(Source[i])) or (str(wordUp) in str(Theme[i]))))):
+                        if author in str(authors[i]):
+                            quotes1.append(quotes[i])
+                            authors1.append(authors[i])
+                            Source1.append(Source[i])
+                            Theme1.append(Theme[i])
+                            NbAvis1.append(NbAvis[i])
+                            Notes1.append(Notes[i])
+        elif word!='' and author =='' and theme !='':
+            wordUp = word[0].capitalize() + word[1:]    #To consider the first capital letter
+            if Nmin != "" and Nmax != "" and Nmin <= Nmax:
+                for i in range(N):
+                    if not(quotes[i] in quotes1) and (((str(word) in quotes[i]) or ((str(word) in str(quotes[i])) or (str(word) in str(authors[i])) or (str(word) in str(Source[i])) or (str(word) in str(Theme[i])))) or ((str(wordUp) in quotes[i]) or ((str(wordUp) in str(quotes[i])) or (str(wordUp) in str(authors[i])) or (str(wordUp) in str(Source[i])) or (str(wordUp) in str(Theme[i]))))):
+                        if theme in str(Theme[i]):
+                            if str(Notes[i])!="nan":
+                                if not (quotes[i] in quotes1) and Nmin<=str(int(Notes[i])) and Nmax>=str(int(Notes[i])): 
+                                    quotes1.append(quotes[i])
+                                    authors1.append(authors[i])
+                                    Source1.append(Source[i])
+                                    Theme1.append(Theme[i])
+                                    NbAvis1.append(NbAvis[i])
+                                    Notes1.append(Notes[i])
+            elif Nmin == "" and Nmax == "" :
+                for i in range(N):
+                    if not(quotes[i] in quotes1) and (((str(word) in quotes[i]) or ((str(word) in str(quotes[i])) or (str(word) in str(authors[i])) or (str(word) in str(Source[i])) or (str(word) in str(Theme[i])))) or ((str(wordUp) in quotes[i]) or ((str(wordUp) in str(quotes[i])) or (str(wordUp) in str(authors[i])) or (str(wordUp) in str(Source[i])) or (str(wordUp) in str(Theme[i]))))):
+                        if theme in str(Theme[i]):
+                            quotes1.append(quotes[i])
+                            authors1.append(authors[i])
+                            Source1.append(Source[i])
+                            Theme1.append(Theme[i])
+                            NbAvis1.append(NbAvis[i])
+                            Notes1.append(Notes[i])
+        elif word!='' and author =='' and theme =='':
+            wordUp = word[0].capitalize() + word[1:]    # To consider the first capital letter
+            if Nmin != "" and Nmax != "" and Nmin <= Nmax:
+                for i in range(N):
+                    if not(quotes[i] in quotes1) and (((str(word) in quotes[i]) or ((str(word) in str(quotes[i])) or (str(word) in str(authors[i])) or (str(word) in str(Source[i])) or (str(word) in str(Theme[i])))) or ((str(wordUp) in quotes[i]) or ((str(wordUp) in str(quotes[i])) or (str(wordUp) in str(authors[i])) or (str(wordUp) in str(Source[i])) or (str(wordUp) in str(Theme[i]))))):
+                        if str(Notes[i])!="nan":
+                            if not (quotes[i] in quotes1) and Nmin<=str(int(Notes[i])) and Nmax>=str(int(Notes[i])): 
+                                quotes1.append(quotes[i])
+                                authors1.append(authors[i])
+                                Source1.append(Source[i])
+                                Theme1.append(Theme[i])
+                                NbAvis1.append(NbAvis[i])
+                                Notes1.append(Notes[i])
+            elif Nmin == "" and Nmax == "" :
+                for i in range(N):
+                    if not(quotes[i] in quotes1) and (((str(word) in quotes[i]) or ((str(word) in str(quotes[i])) or (str(word) in str(authors[i])) or (str(word) in str(Source[i])) or (str(word) in str(Theme[i])))) or ((str(wordUp) in quotes[i]) or ((str(wordUp) in str(quotes[i])) or (str(wordUp) in str(authors[i])) or (str(wordUp) in str(Source[i])) or (str(wordUp) in str(Theme[i]))))):
+                        quotes1.append(quotes[i])
+                        authors1.append(authors[i])
+                        Source1.append(Source[i])
+                        Theme1.append(Theme[i])
+                        NbAvis1.append(NbAvis[i])
+                        Notes1.append(Notes[i])
+                                
+        elif word=='' and author !='' and theme !='':
+            print("1........")
+            if Nmin != "" and Nmax != "" and Nmin <= Nmax:
+                for i in range(N):
+                    if not(quotes[i] in quotes1) :
+                        if author in str(authors[i]):
+                            if theme in str(Theme[i]):
+                                if str(Notes[i])!="nan":
+                                    if not (quotes[i] in quotes1) and Nmin<=str(int(Notes[i])) and Nmax>=str(int(Notes[i])): 
+                                        quotes1.append(quotes[i])
+                                        authors1.append(authors[i])
+                                        Source1.append(Source[i])
+                                        Theme1.append(Theme[i])
+                                        NbAvis1.append(NbAvis[i])
+                                        Notes1.append(Notes[i])
+            elif Nmin == "" and Nmax == "":
+                for i in range(N):
+                    if not(quotes[i] in quotes1):
+                        if author in str(authors[i]):
+                            if theme in str(Theme[i]):
+                                quotes1.append(quotes[i])
+                                authors1.append(authors[i])
+                                Source1.append(Source[i])
+                                Theme1.append(Theme[i])
+                                NbAvis1.append(NbAvis[i])
+                                Notes1.append(Notes[i])
+        elif word=='' and author =='' and theme !='':
+            if Nmin != "" and Nmax != "" and Nmin <= Nmax:
+                for i in range(N):
+                    if not(quotes[i] in quotes1) :
+                        if theme in str(Theme[i]):
+                            if str(Notes[i])!="nan":
+                                if not (quotes[i] in quotes1) and Nmin<=str(int(Notes[i])) and Nmax>=str(int(Notes[i])): 
+                                    quotes1.append(quotes[i])
+                                    authors1.append(authors[i])
+                                    Source1.append(Source[i])
+                                    Theme1.append(Theme[i])
+                                    NbAvis1.append(NbAvis[i])
+                                    Notes1.append(Notes[i])
+            elif Nmin == "" and Nmax == "" :
+                for i in range(N):
+                    if not(quotes[i] in quotes1) and (((str(word) in quotes[i]) or ((str(word) in str(quotes[i])) or (str(word) in str(authors[i])) or (str(word) in str(Source[i])) or (str(word) in str(Theme[i])))) or ((str(wordUp) in quotes[i]) or ((str(wordUp) in str(quotes[i])) or (str(wordUp) in str(authors[i])) or (str(wordUp) in str(Source[i])) or (str(wordUp) in str(Theme[i]))))):
+                        if theme in str(Theme[i]):
+                            quotes1.append(quotes[i])
+                            authors1.append(authors[i])
+                            Source1.append(Source[i])
+                            Theme1.append(Theme[i])
+                            NbAvis1.append(NbAvis[i])
+                            Notes1.append(Notes[i])
+        elif word=='' and author !='' and theme =='':
+            if Nmin != "" and Nmax != "" and Nmin <= Nmax:
+                for i in range(N):
+                    if not(quotes[i] in quotes1) :
+                        if author in str(authors[i]):
+                            if str(Notes[i])!="nan":
+                                if not (quotes[i] in quotes1) and Nmin<=str(int(Notes[i])) and Nmax>=str(int(Notes[i])): 
+                                    quotes1.append(quotes[i])
+                                    authors1.append(authors[i])
+                                    Source1.append(Source[i])
+                                    Theme1.append(Theme[i])
+                                    NbAvis1.append(NbAvis[i])
+                                    Notes1.append(Notes[i])
+            elif Nmin == "" and Nmax == "":
+                for i in range(N):
+                    if not(quotes[i] in quotes1) and (((str(word) in quotes[i]) or ((str(word) in str(quotes[i])) or (str(word) in str(authors[i])) or (str(word) in str(Source[i])) or (str(word) in str(Theme[i])))) or ((str(wordUp) in quotes[i]) or ((str(wordUp) in str(quotes[i])) or (str(wordUp) in str(authors[i])) or (str(wordUp) in str(Source[i])) or (str(wordUp) in str(Theme[i]))))):
+                        if author in str(authors[i]):
+                            if str(Notes[i])!="nan":
+                                quotes1.append(quotes[i])
+                                authors1.append(authors[i])
+                                Source1.append(Source[i])
+                                Theme1.append(Theme[i])
+                                NbAvis1.append(NbAvis[i])
+                                Notes1.append(Notes[i])
+
+        elif word=='' and author =='' and theme =='':
+            if Nmin != "" and Nmax != "" and Nmin <= Nmax:
+                for i in range(N):
+                    if str(Notes[i]) != "nan":
+                        if not (quotes[i] in quotes1) and Nmin <= str(int(Notes[i])) and Nmax >= str(int(Notes[i])):
+
                             quotes1.append(quotes[i])
                             authors1.append(authors[i])
                             Source1.append(Source[i])
@@ -70,40 +224,18 @@ def index():
                             NbAvis1.append(NbAvis[i])
                             Notes1.append(Notes[i])
 
-            elif author != '' and author in str(authors[i]):
-                if theme != '' and theme in str(Theme[i]):
-                    if not (quotes[i] in quotes1):
-                        quotes1.append(quotes[i])
-                        authors1.append(authors[i])
-                        Source1.append(Source[i])
-                        Theme1.append(Theme[i])
-                        NbAvis1.append(NbAvis[i])
-                        Notes1.append(Notes[i])
-            elif theme != '' and theme in str(Theme[i]):
-                if not (quotes[i] in quotes1):
-                    quotes1.append(quotes[i])
-                    authors1.append(authors[i])
-                    Source1.append(Source[i])
-                    Theme1.append(Theme[i])
-                    NbAvis1.append(NbAvis[i])
-                    Notes1.append(Notes[i])
 
-        if len(quotes1) == 0:
+        if len(quotes1) == 0 or (Nmin != "" and Nmax != "" and Nmin > Nmax):
             return resultat(["Pas de résultat :("], ["Sorry"], ["nan"], ["/"], ["nan"], ["nan"])
+
         else:
             return resultat(quotes1, authors1, Source1, Theme1, NbAvis1, Notes1)
     else:
         return render_template("Index.html")
 
-
 @app.route("/resultat/")
 def resultat(quotes, authors, Source, Theme, NbAvis, Notes):
-    return render_template("resultat.html", quotes = quotes, authors = authors, Source = Source, Theme = Theme, NbAvis=NbAvis, Notes=Notes)
-
-@app.route("/page=2")
-def page():
-    return render_template("2.html")
-
+    return render_template("resultat.html", quotes=quotes, authors=authors, Source=Source, Theme=Theme, NbAvis=NbAvis, Notes=Notes)
 
 @app.route("/search")
 def search():
